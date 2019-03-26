f96d3d16b8c642a858c3343f71389d0ff1fd2b04
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-03-26 16:05:03 +0100

    [ADD] New js file to override options

---
+++ tree_default_number_lines/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Coop IT Easy SCRLfs
+# Nicolas Jamoulle, <nicolas@coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Default number of lines in tree",
+    "version": "9.0.1.1.0",
+    "depends": [
+        'base',
+        'web',
+    ],
+    "license": "AGPL-3",
+    "website": "www.coopiteasy.be",
+    "description": """
+        Allow to change default number of lines in journal items
+    """,
+    "data": [
+        'views/view.xml',
+    ],
+    'installable': True,
+}
---
+++ tree_default_number_lines/static/src/js/list_view.js
@@ -0,0 +1,26 @@
+odoo.define('web.ListView.inherited', function (require) {
+"use strict";
+
+    var View = require('web.ListView');
+
+    var ListView = View.include({
+        init: function (parent, dataset, view_id, options) {
+                this._super(parent);
+                this.set_default_options(options);
+        },
+
+        set_default_options: function(options) {
+            this._super(options);
+            _.defaults(this.options, {
+                //whether the editable property of the view has to be disabled
+                'disable_editable_mode': false,
+                'limit': 122,
+                });
+         },
+    }) ;
+//alert('qsc');
+return ListView;});
+
+
+
+
