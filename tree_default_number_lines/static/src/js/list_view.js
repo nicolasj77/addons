08a77e70ad6c3d8b3c04931406aa1831eeba43c0
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-03-28 15:55:30 +0100

    [WIP] tests js

--- tree_default_number_lines/static/src/js/list_view.js
+++ tree_default_number_lines/static/src/js/list_view.js
@@ -3,7 +3,7 @@ odoo.define('web.ListView.inherited', function (require) {
 
     var View = require('web.ListView');
 
-    var ListView = View.extend({
+    var ListView = View.include({
         init: function (parent, dataset, view_id, options) {
                 this._super(parent);
                 this.set_default_options(options);
@@ -13,7 +13,6 @@ odoo.define('web.ListView.inherited', function (require) {
             this._super(options);
             _.defaults(this.options, {
                 //whether the editable property of the view has to be disabled
-                'disable_editable_mode': false,
                 'limit': 122,
                 });
          },
